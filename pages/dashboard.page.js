342fae90987aef22441e20fa6e7b3036bd052fc7
Cristhian Arispe <cristhian.arispe@inallmedia.com>
2026-06-01 21:23:36 -0400

    changes on dashboard

--- pages/dashboard.page.js
+++ pages/dashboard.page.js
@@ -4,9 +4,11 @@ export class DashboardPage extends BasePage {
     constructor(page) {
         super(page);
 
-        this.addTaskButton = page.getByTestId('upcoming-view-list-layout').getByRole('button', { name: 'Add task' });
+        //this.addTaskButton = page.getByTestId('upcoming-view-list-layout').getByRole('button', { name: 'Add task' });
+        this.addTaskButton = page.locator('button').filter({ hasText: 'Add task' }).first();
         this.taskNameInput = page.getByRole('textbox', { name: 'Task name', exact: true });
-        this.createTaskSubmitButton = page.getByTestId('task_list_editor_wrapper').getByTestId('task-editor-submit-button');
+        //this.createTaskSubmitButton = page.getByTestId('task_list_editor_wrapper').getByTestId('task-editor-submit-button');
+        this.createTaskSubmitButton = page.getByTestId('task-editor-submit-button').first();
         this.taskEditorSubmitButton = page.getByTestId('task-detail-editor-container').getByTestId('task-editor-submit-button');
         this.taskDetailsModal = page.getByTestId('task-details-modal');
         this.taskNameButton = page.getByRole('button', {name: 'Task name', exact: true});
